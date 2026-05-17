b5b3895d675ed2fe65e0c32390f790cb1a63956f
SaturnusOps <harmandeeptung2@gmail.com>
2026-05-17 14:46:30 +0200

    Get User Input

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wie lang soll die Line gezogen weden:");
+        int eigegebeneZahl = scanner.nextInt();
+
+    }
+}
